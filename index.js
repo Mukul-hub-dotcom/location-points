1b5a7246b4ff45c5662bc4ecfb561852b4d43be9
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2022-12-30 01:12:26 +0530

    done

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.get("/", (req, res) => {
     let pos = []
     pos[0] = {
         name: "origin",
-        lat: Math.random() * (55 - 51) + 51,
+        lat: Math.random() * (55 - 53) + 53,
         lng: Math.random() * (94 - 91) + 91,
     }
     for (let i = 0; i < 2; i++) {
         pos[i + 1] = {
             name: i + 1,
-            lat: Math.random() * (55 - 51) + 51,
+            lat: Math.random() * (55 - 53) + 53,
             lng: Math.random() * (95 - 91) + 91,
         }
     }
